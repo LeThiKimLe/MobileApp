6faf4c0d18052738bb43c93c0f467963d45ab0e6
Nguyễn Thị Cẩm Nguyên <20110315@student.hcmute.edu.vn>
2023-05-18 09:24:35 +0700

    Load lại trag mycourse sau khi sửa APIhocvien

--- app/src/main/java/vn/iotstar/finalproject/Retrofit/RetrofitClient.java
+++ app/src/main/java/vn/iotstar/finalproject/Retrofit/RetrofitClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
     private static Retrofit retrofit1, retrofit2;
 
-    private static final String BASE_URL1 = "http://192.168.137.1:8080/WebCuoiKy/";
+    private static final String BASE_URL1 = "http://192.168.25.191:8080/WebCuoiKy/";
     public static Retrofit getRetrofit() {
         if(retrofit1 == null) {
             retrofit1 = new Retrofit.Builder()
