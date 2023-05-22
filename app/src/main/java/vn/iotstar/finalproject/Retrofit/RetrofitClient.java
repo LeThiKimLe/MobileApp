66dea52b2364a35cb3df643caded11c9748801c9
KimLe <kimle020102@gmail.com>
2023-05-22 14:40:00 +0700

    Fix lesson function

--- app/src/main/java/vn/iotstar/finalproject/Retrofit/RetrofitClient.java
+++ app/src/main/java/vn/iotstar/finalproject/Retrofit/RetrofitClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit1, retrofit2;
-    private static final String BASE_URL1 = "http://192.168.1.3:8080/WebCuoiKy/";
+    private static final String BASE_URL1 = "http://192.168.0.100:8282/WebCuoiKy/";
     public static Retrofit getRetrofit() {
         if(retrofit1 == null) {
             retrofit1 = new Retrofit.Builder()
